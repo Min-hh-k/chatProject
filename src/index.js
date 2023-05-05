81c2c68a2b57bf7f335e51710f21de6047714fc1
Min Hyeok <96058996+Min-hh-k@users.noreply.github.com>
2023-05-05 21:10:58 +0900

    feat : redux 셋팅 1차

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import rootReducer from "./redux/reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// 스토어 생성
+const createStoreMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* redux 크롬익스텐션 사용 위하여 */}
+    <Provider
+      store={createStoreMiddleware(
+        rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX__DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+// import user from './userReducer'
+// import chatroom from './chatroomReducer'
+
+const rootReducer = combineReducers({
+  // user, chatroom
+})
+
+export default rootReducer
